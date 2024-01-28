75dac14c9051c4063d65710e6ad27238dada8852
Dalinte <kollekcionerssd1@gmail.com>
2024-01-28 12:51:46 +0300

    change project name, add cli script

---
+++ bin/cli.js
@@ -0,0 +1,33 @@
+#!/usr/bit/env node
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { execSync } = require('child_process');
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (e) {
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/Dalinte/React-TypeScript-Vite-FSD.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && yarn install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+
+const installDeps = runCommand(installDepsCommand);
+
+if (!installDeps) process.exit(-1);
+
+console.log('Congratulations!');
